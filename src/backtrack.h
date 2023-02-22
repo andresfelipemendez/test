f4357077d5cdcd62f5e0eba5a4bb2436fc51321e
Andres Mendez <fmendez1212@gmail.com>
2023-02-22 18:31:56 -0500

    test scafold

---
+++ src/backtrack.h
@@ -0,0 +1,8 @@
+#pragma once
+
+#include <string>
+
+std::string backTrack(std::string input) {
+    return "hi";
+}
+
--- src/hello_test.cc
+++ src/hello_test.cc
@@ -1,5 +1,7 @@
 #include <gtest/gtest.h>
 
+#include "backtrack.h"
+
 // Demonstrate some basic assertions.
 TEST(HelloTest, BasicAssertions) {
     // Expect two strings not to be equal.
@@ -9,8 +11,5 @@ TEST(HelloTest, BasicAssertions) {
 }
 
 TEST(HelloTest1, BasicAssertions) {
-    // Expect two strings not to be equal.
-    EXPECT_STRNE("hello", "world");
-    // Expect equality.
-    EXPECT_EQ(7 * 7, 42);
+    EXPECT_EQ(backTrack("_"), "hi");
 }
