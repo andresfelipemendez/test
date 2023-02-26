fc801c5581623e286c67d8cef2e140e5df6b924c
Andres Mendez <fmendez1212@gmail.com>
2023-02-26 18:58:01 -0500

    back track prune

--- src/backtrack.h
+++ src/backtrack.h
@@ -1,26 +1,12 @@
-#pragma once
-
+#include <iostream>
 #include <string>
 #include <vector>
 #include <utility>
 #include <algorithm>
+#include <sstream>
 #include <numeric>
 
 using ListOfPairs = std::vector<std::pair<std::string,std::string>>;
-
-std::vector<std::string> splitInput(const std::string& input) {
-    std::vector<std::string> inputs;
-    char const* digits = "0123456789";
-    std::size_t n = input.find_first_of(digits);
-    while (n != std::string::npos)
-    {
-        std::size_t const m = input.find_first_not_of(digits, n);
-        n = input.find_first_of(digits,m);
-        auto res = input.substr(m, n != std::string::npos ? n-m : m);
-        inputs.push_back(res);
-    }
-    return inputs;
-}
 
 std::vector<std::string> splitString(const std::string& input, char delimiter){
     std::string string;
@@ -32,17 +18,6 @@ std::vector<std::string> splitString(const std::string& input, char delimiter){
     return strings;
 }
 
-std::vector<std::pair<std::string,std::string>> listOfPairs(const std::string& input) {
-    std::vector<std::pair<std::string,std::string>> pairs;
-    auto lines = splitString(input,'\n');
-    auto it = ++begin (lines); // start from second line
-    for (; it != end (lines); ++it) {
-        auto word = splitString(*it,' ');
-        auto pair = std::make_pair(word[0],word[1]);
-        pairs.push_back(pair);
-    }
-    return pairs;
-}
 
 bool isValidState(const std::vector<int>& state, const ListOfPairs& listOfPairs) {
     std::string concatenatedLeft, concatenatedRight;
@@ -55,27 +30,92 @@ bool isValidState(const std::vector<int>& state, const ListOfPairs& listOfPairs)
     return areEqual && areNotEmpty;
 }
 
-std::vector<int> getCandidates(std::vector<int> state, const ListOfPairs& listOfPairs) {
+std::vector<int> prune(const std::vector<int>& potentialCandidates, const std::vector<int>& state, const ListOfPairs& listOfPairs) {
     std::vector<int> candidates;
-    std::vector<int> a(listOfPairs.size());
-    std::iota(a.begin(), a.end(), 0);
-    if(state.empty()) {
-        return a;
+    std::string first, second;
+    for(int pairIndex : state){
+        first += listOfPairs[pairIndex].first;
+        second += listOfPairs[pairIndex].second;
+    }
+    // I need the "overlapping" substring to match in the next pair on the oppoite side
+    std::string overlappingSubstring;
+    int side = 0;
+    if(first.size() < second.size()) {
+        overlappingSubstring = second.substr(first.size(), second.size() - first.size());
+        side = 1;
+    } else if (first.size() > second.size()) {
+        overlappingSubstring = first.substr(second.size(), first.size() - second.size());
+        side = 2;
+    } else if (first.size() == second.size()){
+        overlappingSubstring = first;
+        side = 3;
     }
 
-    for(int i : a) {
-        auto it = std::find(state.begin(),state.end(),i);
-        if(it == state.end()){
-            candidates.push_back(i);
+    for(int potentialCandidate : potentialCandidates) {
+        auto pair = listOfPairs[potentialCandidate];
+        if(side == 1) {
+            std::string overlappingOppositeSubstring = pair.first.substr(0, overlappingSubstring.size());
+            if(overlappingSubstring == overlappingOppositeSubstring)
+                candidates.push_back(potentialCandidate);
+        } else if (side == 2) {
+            std::string overlappingOppositeSubstring = pair.second.substr(0, overlappingSubstring.size());
+            if(overlappingSubstring == overlappingOppositeSubstring)
+                candidates.push_back(potentialCandidate);
+        } else if (side == 3 ) {
+            // check what string can fit the substring?
+            std::string overlappingOppositeSubstring = pair.second.substr(0, overlappingSubstring.size());
+            if(overlappingSubstring == overlappingOppositeSubstring)
+                candidates.push_back(potentialCandidate);
         }
     }
+
 
     return candidates;
 }
 
-bool search(std::vector<int>& state, std::vector<std::vector<int>> &solutions, const ListOfPairs& listOfPairs) {
-    if(isValidState(state, listOfPairs))
+bool compareSubsetString(const std::string& first, const std::string& seccond) {
+    if(first.size() == seccond.size())
+        return first == seccond;
+    const auto& shortestString = first.size() > seccond.size() ? seccond : first;
+    const auto& longestString = first.size() < seccond.size() ? seccond : first;
+    const auto subString = longestString.substr(0,shortestString.size());
+    return subString == shortestString;
+}
+
+std::vector<int> getCandidates(std::vector<int> state, const ListOfPairs& listOfPairs) {
+    std::vector<int> candidates;
+
+    if(state.empty()) {
+        for (int i = 0; i < listOfPairs.size(); ++i) {
+            const auto& pair = listOfPairs[i];
+
+            if(compareSubsetString(pair.first, pair.second)) {
+                candidates.push_back(i);
+            }
+        }
+        return candidates;
+    }
+
+    std::vector<int> potentialCandidates, a(listOfPairs.size());
+    std::iota(a.begin(), a.end(), 0);
+    for(int i : a) {
+        auto it = std::find(state.begin(),state.end(),i);
+        if(it == state.end()){
+            potentialCandidates.push_back(i);
+        }
+    }
+
+    candidates = prune(potentialCandidates, state, listOfPairs);
+
+
+    return candidates;
+}
+
+void search(std::vector<int>& state, std::vector<std::vector<int>> &solutions, const ListOfPairs& listOfPairs) {
+    if(isValidState(state, listOfPairs)){
         solutions.push_back(state);
+        return;
+    }
 
     auto candidates = getCandidates(state, listOfPairs);
     for(auto& candidate : candidates){
@@ -85,12 +125,13 @@ bool search(std::vector<int>& state, std::vector<std::vector<int>> &solutions, c
     }
 }
 
+
 std::string solve(const ListOfPairs& listOfPairs) {
     std::vector<std::vector<int>> solutions {};
     std::vector<int> state {};
+
     search(state, solutions,listOfPairs);
     if (solutions.empty()){
-        // impossible
         return "IMPOSSIBLE";
     }
 
@@ -114,15 +155,55 @@ std::string solve(const ListOfPairs& listOfPairs) {
     return concatenatedSolutions[0];
 }
 
-std::vector<std::string> solveEmilPuzzle(std::string rawInput) {
-    auto splitedInput = splitInput(std::move(rawInput));
+std::vector<std::vector<std::pair<std::string,std::string>>> processInput(const std::string& input) {
+    // number of input groups
+    std::vector<std::vector<std::pair<std::string,std::string>>> listOfGroupsOfPairs;
+    auto splitted = splitString(input, '\n');
+
+    int numberOfPairs = 0;
+    auto it = listOfGroupsOfPairs.begin();
+    for(std::string line : splitted) {
+        if(numberOfPairs == 0) {
+            numberOfPairs = std::stoi(line);
+            listOfGroupsOfPairs.push_back({});
+            it = listOfGroupsOfPairs.end() - 1;
+        } else {
+            auto pair = splitString(line, ' ');
+            it->push_back({pair[0], pair[1]});
+            numberOfPairs--;
+        }
+    }
+
+    return listOfGroupsOfPairs;
+}
+
+std::vector<std::string> solveEmilPuzzle(const std::string& rawInput) {
+    auto processedInput = processInput(rawInput);
     std::vector<std::string> solutions;
-    for(const auto& input : splitedInput){
-        auto pairs = listOfPairs(input);
-        auto solution = solve(pairs);
+
+    for(const auto& pairGroup : processedInput){
+        auto solution = solve(pairGroup);
         solutions.push_back(solution);
     }
 
     return solutions;
 }
 
+//int main(int argc, char** argv) {
+//    std::string inputString;
+//    while(std::cin) {
+//        std::string line;
+//        std::getline(std::cin, line);
+//        inputString += line + "\n";
+//    }
+//
+//    auto solutions = solveEmilPuzzle(inputString);
+//
+//    int caseNumber = 1;
+//    for(const auto& solution : solutions) {
+//        //    std::cout << solution << "\n";
+//        std::cout << "Case " << caseNumber++ << ": " << solution << "\n";
+//    }
+//
+//    return 0;
+//}
--- src/hello_test.cc
+++ src/hello_test.cc
@@ -2,8 +2,19 @@
 
 #include "backtrack.h"
 
-TEST(HelloTest1, BasicAssertions) {
-    std::string input = R"(5
+std::string input = R"(11
+in inee
+eedane da
+xampl  nexamp
+es les
+tri tringtha
+ngtha tha
+thas smu
+mult tiple
+ip in
+leinpu puts
+supto upto
+5
 are yo
 you u
 how nhoware
@@ -27,5 +38,52 @@ a ab
 b bb
 c cc
 )";
+
+TEST(processInput, SplittInputGroups) {
+    auto processedInput = processInput(input);
+    EXPECT_EQ(processedInput.size(), 5);
+    EXPECT_EQ(processedInput[0].size(), 11);
+
+}
+
+TEST(processInput, SplittInputString) {
+    auto processedInput = splitString("in inee", ' ');
+    EXPECT_EQ(processedInput.size(), 2);
+    EXPECT_EQ(processedInput[0], "in");
+    EXPECT_EQ(processedInput[1], "inee");
+}
+
+TEST(solveEmilPuzzle, solveAllCases) {
     EXPECT_EQ(solveEmilPuzzle(input)[0], "hi");
+}
+
+TEST(solveEmilPuzzle, solveFirstCase) {
+    auto pairGroup = processInput(input)[0];
+    EXPECT_EQ(solve(pairGroup), "hi");
+}
+
+TEST(solveEmilPuzzle, getCandidates) {
+    auto state = std::vector<int> {};
+    auto listofpairs = std::vector<std::pair<std::string,std::string>> {
+            /*0*/{"in", "inee"},
+            /*1*/{"eedane", "da"},
+            /*2*/{"xampl", " nexamp"},
+            /*3*/{"es", "les"},
+            /*4*/{"tri", "tringtha"},
+            /*5*/{"ngtha", "tha"},
+            /*6*/{"thas", "smu"},
+            /*7*/{"mult", "tiple"},
+            /*8*/{"ip", "in"},
+            /*9*/{"leinpu", "puts"},
+            /*10*/{"supto", "upto"},
+    };
+    auto candidates = getCandidates(state, listofpairs);
+    EXPECT_EQ(candidates.size(), 2);
+    EXPECT_EQ(candidates[0], 0);
+    EXPECT_EQ(candidates[1], 4);
+}
+
+TEST(solveEmilPuzzle, compareSubsetString) {
+    bool res = compareSubsetString("ip", "in");
+    EXPECT_FALSE(res);
 }
